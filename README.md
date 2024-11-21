304884a4b09aca0e6561e31f2cb88669063291db
sootopia <shpark7502@naver.com>
2024-11-21 15:58:21 +0900

    feat: 첫 커밋

---
+++ README.md
@@ -0,0 +1,1 @@
+## AWS Lightsail LAMP CI/CD
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AWS Lightsail LAMP8 CI/CD Test</title>
+</head>
+
+<body>
+    <h1>Hello</h1>
+</body>
+
+</html>
